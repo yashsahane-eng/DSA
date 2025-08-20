7e0073de12143f0d6e8e2e38b1d843be8cf97bd4
yashsahane-eng <114651935+yashsahane-eng@users.noreply.github.com>
2025-08-20 22:49:48 +0530

    [541] [Time Beats: 100.00%] [Memory Beats: 77.64%] - LeetPush

--- Reverse-String-II.cpp
+++ Reverse-String-II.cpp
@@ -2,27 +2,14 @@ class Solution {
 public:
     string reverseStr(string s, int k) {
         int n = s.size();
-        int size = 2 * k;
-        int temp = k;
-        int i = 0;
 
-        if(n < k || n == k){
-            reverse(s.begin(), s.end());
-            return s;
-        }
-
-        while(i < n){
-            reverse(s.begin() + i, s.begin() + temp);
-            i += size;
-            temp += (2 * k);
-            if(k == n) break;
-            if((n - i) < (2 * k) && (n - i) >= k){
-                reverse(s.begin() + i, s.begin() + temp);
-                return s;
-            }
-            if(n - i < k){
-                reverse(s.begin() +i, s.end());
-                return s;
+        for (int i = 0; i < n; i += 2 * k) {
+            
+            if (i + k <= n) {
+                reverse(s.begin() + i, s.begin() + i + k);
+            } else {
+                
+                reverse(s.begin() + i, s.end());
             }
         }
         return s;
