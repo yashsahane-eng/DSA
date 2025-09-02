84e07a66bd4e88d6c0a9f0c3c4e29237d37ea497
yashsahane-eng <114651935+yashsahane-eng@users.noreply.github.com>
2025-09-02 11:35:56 +0530

    [383] [Time Beats: 60.26%] [Memory Beats: 51.73%] - LeetPush

---
+++ Ransom-Note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if(ransomNote.size()>magazine.size()) return false;
+
+        vector<int> count(26 ,0);
+        for(char c:magazine){
+            count[c-'a']++;
+        }
+
+        for(char c : ransomNote){
+            count[c-'a']--;
+            if(count[c-'a']<0){
+                return false;
+            }
+        }
+        return true;
+        
+        
+
+
+    }
+};
