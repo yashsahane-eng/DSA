54d19786c29cf8538e3a9fbc4c4f8787dbbbbc91
yashsahane-eng <114651935+yashsahane-eng@users.noreply.github.com>
2025-09-25 13:55:37 +0530

    [704] [Time Beats: 100.00%] [Memory Beats: 9.45%] - LeetPush

--- Binary-Search.cpp
+++ Binary-Search.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
+        int n=nums.size();
 
-        int low = 0, high = n - 1;
+        int left=0 , right=n-1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2; 
+        while(left<=right){
+            int mid=left+(right-left)/2;
 
-            if (nums[mid] == target) 
+            if (nums[mid]==target){
                 return mid;
+            }
+            else if (nums[mid]<target){
+                left=mid+1;
 
-            else if (target > nums[mid]) 
-                low = mid + 1;
+            }
+            else right=mid-1;
 
-            else 
-                high = mid - 1;
+
         }
-        return -1; 
+        return -1;
     }
 };
