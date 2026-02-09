28a82c9fea211b194678ab5d7a3f1a12a86c71a5
yashsahane-eng <114651935+yashsahane-eng@users.noreply.github.com>
2026-02-09 23:07:12 +0530

    [1] [Time Beats: 7.36%] [Memory Beats: 66.20%] - LeetPush

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -1,18 +1,18 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
-            }
-        }
-        return {-1, -1};
-    }
-};
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        int n = nums.size();
+5
+6        vector<int> ans;
+7        for (int i = 0; i < n; i++) {
+8            for (int j = i + 1; j < n; j++) {
+9                if (nums[i] + nums[j] == target) {
+10                    ans.push_back(i);
+11                    ans.push_back(j);
+12                    return ans;
+13                }
+14            }
+15        }
+16        return {-1, -1};
+17    }
+18};
