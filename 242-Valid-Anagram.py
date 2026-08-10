4950a59eff7d217a1608d6ddc71da6e7dd5caa5d
Er . Yash Sahane <114651935+yashsahane-eng@users.noreply.github.com>
2026-08-10 12:21:53 +0530

    [242] [Time Beats: 77.38%] [Memory Beats: 75.86%] - LeetPush

--- 242-Valid-Anagram.py
+++ 242-Valid-Anagram.py
@@ -1,11 +1,8 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if (len(s) != len(t)):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+
+        if len(s) !=len(t):
             return False
 
         freqS={}
@@ -20,9 +17,10 @@ class Solution(object):
         for ch in t:
             if ch in freqT:
                 freqT[ch]+=1
+
             else:
                 freqT[ch]=1
 
+        
         return freqS==freqT
-
         
